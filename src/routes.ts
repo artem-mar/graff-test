2f91c0db39a89937380fa50be9a31cf8bc47fa16
Artem M <artemmarin91@mail.ru>
2023-04-01 03:00:32 +0300

    added route for fetching user by id

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ const storeApi = 'https://fakestoreapi.com';
 
 const routes = {
   productsApi: () => `${storeApi}/products`,
+  productByIdApi: (id: number) => `${storeApi}/products/${id}`,
 
   listPage: () => '/',
   cardPage: (id: number | string = '') => (id ? `product?id=${id}` : 'product'),
